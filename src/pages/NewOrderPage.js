934a47453dcc6c5908a20672c4726b6a32217641
AbubakarSiddiquePA <siddiquepanathur@gmail.com>
2026-02-05 14:34:39 +0400

    Remove BOM from pages

--- src/pages/NewOrderPage.js
+++ src/pages/NewOrderPage.js
@@ -1,4 +1,4 @@
-﻿import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
